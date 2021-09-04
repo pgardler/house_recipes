8d88fddf871cae7661edf63ed468876cbd604a0b
Dylan <dylanb0708@gmail.com>
2021-09-04 13:46:08 -0400

    Added bootstrap, dt, and a home controller.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,12 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "../stylesheets/application";
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+require( 'jquery' );
+require( 'datatables.net-bs4' );
+require( 'datatables.net-responsive-bs4' );
